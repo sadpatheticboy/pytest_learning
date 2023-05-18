0dddf5c213c53daf36c046a020439d4fc7c36001
sadpatheticboy <bal-tim@mail.ru>
2023-05-19 00:20:58 +0300

    Попробовал поработать с фикстурами в рамках текстового файла.

--- tests/test_division_func.py
+++ tests/test_division_func.py
@@ -1,4 +1,4 @@
-from utils import division
+from my_funcs.utils import division
 import pytest
 
 
@@ -11,7 +11,7 @@ def test_division_good(a, b, expected_result):
 
 
 @pytest.mark.parametrize('expected_excpetion, dividend, divider', [(ZeroDivisionError, 10, 0),
-                                                         (TypeError, 20, '2')])
+                                                                   (TypeError, 20, '2')])
 def test_zero_division(expected_excpetion, dividend, divider):
     with pytest.raises(expected_excpetion):
         division(dividend, divider)
